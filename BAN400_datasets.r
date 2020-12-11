8f53915f8a9c1fc08dc52653f4c0bebc1978b0d4
Tonje <tonje.hellestol@student.uib.no>
2020-12-11 11:50:44 +0100

    lagt til korrigering for negative daily tilfeller i crossgov

--- BAN400_datasets.r
+++ BAN400_datasets.r
@@ -38,6 +38,7 @@ column_names = c("ID", "date", "confirmed_cases", "confirmed_deaths", "country_n
 #   replace(is.na(.),0) %>%
 #   mutate(daily_cases = c(0,diff(confirmed_cases)), daily_deaths = c(0,diff(confirmed_deaths)))
 
+na.locf2 <- function(x) na.locf(x, na.rm = FALSE)
 
 #Forslag fra Tonje - for ?? fikse litt mer p?? NAs. Usikker p?? hvordan det b??r l??ses
 Crossgovsources_df <- covid19() %>% 
@@ -50,6 +51,12 @@ Crossgovsources_df <- covid19() %>%
   mutate(daily_cases = c(0,diff(confirmed_cases)), daily_deaths = c(0,diff(confirmed_deaths)))
 
 
+#Adding column "negative_daily_cases" and "negative_daily_deaths", holds the value 1 if daily_cases/daily_deaths are negative, 0 otherwise
+Crossgovsources_df <- Crossgovsources_df %>% group_by(country_name) %>% mutate(negative_daily_cases = (ifelse( daily_cases < 0, 1, 0)), negativ_daily_deaths = (ifelse( daily_deaths < 0, 1, 0))) %>% ungroup()
+
+#Correction: changing negative daily_deaths and negative daily_cases to 0
+Crossgovsources_df <- Crossgovsources_df %>% 
+  mutate( daily_deaths = replace(daily_deaths , daily_deaths < 0, 0), daily_cases = replace(daily_deaths , daily_cases < 0, 0))
 
 
 #Datasettet ECDC blir ikke oppdatert regelmessig, g?r fullstendig over til daglige oppdateringer 14 des, denne b?r dermed ikke brukes
@@ -91,6 +98,9 @@ JHD_df_full <- JHD_df_confirmed %>%
   rename(country_name = Country.Region) %>% 
   mutate(date=as.Date(date, format = "%Y-%m-%d"), country_name = as.character(country_name)) %>% 
   mutate(daily_cases = c(0,diff(confirmed_cases)), daily_deaths = c(0,diff(confirmed_deaths)))
+
+
+
 
 
 ####### Finished retrieving datasets
